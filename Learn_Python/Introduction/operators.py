6d95d73d5fb031d743103db97a55fb5e8ef2ecc9
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-14 14:13:39 +0530

    PEMDAS

--- Learn_Python/Introduction/operators.py
+++ Learn_Python/Introduction/operators.py
@@ -1,5 +1,5 @@
 # Operators
-
+# PEMDAS - Parentheses, Exponents, Multiplication and Division (from left to right), Addition and Subtraction (from left to right)
 a = 5
 b = 10
 
@@ -10,3 +10,8 @@ print(a * b) # Multiplication
 print(a / b) # Division
 print(a % b) # Modulus
 print(a ** b) # Exponentiation
+
+result = (a + b) * 2 
+print(result)
+
+# Assignment Operators
