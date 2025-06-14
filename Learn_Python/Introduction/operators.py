bbbc707ee9092e5274e34f24a11609afb51c0a9b
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-14 14:11:43 +0530

    Arithmetic Operators

---
+++ Learn_Python/Introduction/operators.py
@@ -0,0 +1,12 @@
+# Operators
+
+a = 5
+b = 10
+
+# Arithmetic Operators
+print(a + b) # Addition
+print(a - b) # Subtraction
+print(a * b) # Multiplication
+print(a / b) # Division
+print(a % b) # Modulus
+print(a ** b) # Exponentiation
