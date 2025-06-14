1183eedc5cae92df7b035dfefd2e92f7e4c908a0
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-14 14:10:26 +0530

    dictionary example in Python

--- Learn_Python/Introduction/data-types.py
+++ Learn_Python/Introduction/data-types.py
@@ -26,28 +26,25 @@ print(text)
 # List - ordered, mutable, allows duplicates
 my_list = [1,2,3,4,5] #list [1,2,3,4,5]
 print(my_list)
-print(type(my_list))
-
 # Tuple - ordered, immutable, allows duplicates
 my_tuple = (1,2,3,4,5) #tuple (1,2,3,4,5)
 print(my_tuple)
-print(type(my_tuple))
 
 # Set - unordered, mutable, no duplicates
 my_set = {1,2,3,4,5} #set {1,2,3,4,5}
 print(my_set)
-print(type(my_set))
 
 # frozenset - unordered, immutable, no duplicates
 my_frozenset = frozenset({1,2,3,4,5}) #frozenset {1,2,3,4,5}
 print(my_frozenset)
-print(type(my_frozenset))
+
 
 # Dictionary - unordered, mutable, key-value pairs
-my_dict = {"name": "John", "age": 30} #dictionary {"name": "John", "age": 30}
+my_dict = {"name": "John", "age": 30, "city": "New York"} #dictionary 
 print(my_dict)
-print(type(my_dict))
-
+print(my_dict["name"])
+print(my_dict["age"])
+print(my_dict["city"])
 
 
 
