64df13e283f78f1edecd8717a8b3e24526f594e8
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-15 08:53:01 +0530

    Add examples of loop control statements: break, continue, nested loops, and range function

---
+++ Learn_Python/Introduction/Control Statements/loops/break.py
@@ -0,0 +1,4 @@
+for i in range(1, 10):
+    if i == 5:
+        break #stop the loop
+    print(i)
---
+++ Learn_Python/Introduction/Control Statements/loops/range.py
@@ -0,0 +1,12 @@
+"""
+range(start, stop, step)
+start - inclusive
+stop - exclusive
+step - increment by default 1
+
+""" 
+
+for i in range(1, 10, 2):
+    print(i)
+
+
