b6d7f9777eadbc8bffd49cd0fef419e60a0f1f2d
Md Noorullah Raza <mnraza94@gmail.com>
2025-06-15 09:29:19 +0530

    Add new string checking startswith, endswith, isalpha, isdigit, isalnum, isspace, isprintable, and isidentifier.

---
+++ Learn_Python/Introduction/Strings/Checking Methods/check.py
@@ -0,0 +1,10 @@
+str = 'Python programming'
+print(str.startswith('Python')) #Check if the string starts with the given substring
+
+print(str.endswith('programming')) #Check if the string ends with the given substring
+print(str.isalpha()) #Check if the string is alphabetic
+print(str.isdigit()) #Check if the string is numeric
+print(str.isalnum()) #Check if the string is alphanumeric
+print(str.isspace()) #Check if the string is a space
+print(str.isprintable()) #Check if the string is printable
+print(str.isidentifier()) #Check if the string is a valid identifier
